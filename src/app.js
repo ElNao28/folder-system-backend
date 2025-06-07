8c3b58754c154092a28dd599cdb339c98cbd93ae
luis jair <jair.escalante@phs.mx>
2025-06-07 02:19:01 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import folderRoutes from "./routes/folder.route.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
